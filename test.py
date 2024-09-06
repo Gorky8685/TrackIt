477ec26be1eddd42bce1ed5dbe31343a59398512
Gorky8685 <826769080@qq.com>
2024-09-06 13:51:07 +0800

    install

---
+++ test.py
@@ -0,0 +1,9 @@
+"""
+路网数据要求！
+路网点层数据和线层数据的坐标系必须为：EPSG:4326
+node_id link_id 字段类型int
+dir=1 代表路段拓扑正向
+link_id，from_node，to_node，dir，length，geometry (LINESTRING (108.84048418194278 34.208751404812496, 108.8410333043887 34.20538952458989))
+可以再加个road_name
+
+"""
